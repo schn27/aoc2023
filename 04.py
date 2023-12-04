9b0573fe6cd3fb99ee8bfc782c00e8ca1ea3864b
Aleksandr Malikov <schn27@gmail.com>
2023-12-04 11:15:24 +0300

    day 04

---
+++ 04.py
@@ -0,0 +1,24 @@
+from aocd import data
+from aocd import submit
+import re
+
+cards = list(map(
+    lambda line: list(map(
+        lambda s: list(map(int, re.findall(r'\d+', s))),
+        line.split(':')[1].split('|'))),
+    data.split('\n')))
+
+def get_score(matched):
+    return 2 ** (matched - 1) if matched > 0 else 0
+
+cards = list(map(lambda c: len(list(filter(lambda r: r in c[0], c[1]))), cards))
+
+submit(sum(map(get_score, cards)), part='a')
+
+copies = [1] * len(cards)
+
+for i, c in enumerate(cards):
+    for j in range(i, min(i + c, len(cards) - 1)):
+        copies[j + 1] += copies[i]
+
+submit(sum(copies), part='b')
